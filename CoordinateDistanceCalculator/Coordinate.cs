668cf7ac54307ca1d59ce2cd6e5c7fdce4e90822
Muhammadjon <rahmatullayevmuhammadjon042@gmail.com>
2024-10-14 12:22:12 +0500

    Добавьте файлы проекта.

---
+++ CoordinateDistanceCalculator/Coordinate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoordinateDistanceCalculator
+{
+    public struct Coordinate
+    {
+      
+        public Coordinate(double latitude, double longitude)
+        {
+            this.Latitude = latitude;
+            this.Longitude = longitude;
+        }
+        public Coordinate(string? s)
+        {
+
+           var collict = s?.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(Convert.ToDouble).ToArray();
+            if (collict is { Length: 2 })
+            {
+                Latitude = collict[0];
+                Longitude = collict[1];
+            }
+            else
+            {
+                Latitude = 0;
+                Longitude = 0;
+            }
+           
+        }
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+
+        public static Coordinate From(string? s)
+        {
+          return new Coordinate(s);
+        }
+    }
+}
---
+++ CoordinateDistanceCalculator/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace CoordinateDistanceCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Birinchi manzilni kiriting : ");
+            var Start = Coordinate.From(Console.ReadLine());
+            Console.WriteLine("Ikkinchi manzilni kiriting: ");
+            var Finish = new Coordinate(Console.ReadLine());
+         
+            double distance = HaversineDistance(Start, Finish);
+            distance = Math.Round(distance, 2);
+            Console.WriteLine($"Ikkita nuqta orasidagi masofa: {distance} m");
+        }
+        public static double HaversineDistance(Coordinate start, Coordinate finish)
+        {
+            const double R = 6371000;
+
+            // Kenglik va uzunliklarni radianga aylantirish
+            double dLat = ToRadians(finish.Latitude - start.Latitude);
+            double dLon = ToRadians(finish.Longitude - start.Longitude);
+
+            
+            double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                       Math.Cos(ToRadians(start.Latitude)) * Math.Cos(ToRadians(finish.Latitude)) *
+                       Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+            return R * c; 
+        }
+
+       
+        public static double ToRadians(double angle)
+        {
+            return angle * Math.PI / 180.0;
+        }
+
+    }
+
+
+}
